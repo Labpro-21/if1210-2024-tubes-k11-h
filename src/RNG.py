da8e5b08acb2fb2b329f7755614f6c99ab6d6ec6
19623110 <163651040+19623110@users.noreply.github.com>
2024-05-17 18:36:50 +0700

    fix: re-add RNG and related functions

---
+++ src/RNG.py
@@ -0,0 +1,54 @@
+import time
+from globevar import user_data,monster_inventory_data,item_inventory_data,monster_data,arr_item,sudah_login
+from utils import search_index,parser,printDict
+
+#REALISASI FUNGSI-FUNGSI
+
+def get_seed():
+    """Menghasilkan seed berdasarkan waktu saat ini."""
+    return int(time.time())
+
+# Fungsi untuk Linear Congruential Method
+def linear_congruential_method_int(seed, a, c, m, n, min_val, max_val):
+    """
+    Menghasilkan urutan bilangan bulat acak semu dalam rentang tertentu
+    menggunakan Metode Kongruensial Linier (LCM).
+    
+    Parameter
+        seed (int): Nilai benih awal.
+        a (int): Pengali.
+        c (int): Kenaikan.
+        m (int): Modulus.
+        n (int): Jumlah angka acak yang akan dihasilkan.
+        min_val (int): Nilai minimum dari rentang.
+        max_val (int): Nilai maksimum dari rentang.
+        
+    Pengembalian:
+        daftar Daftar bilangan bulat acak semu dalam rentang yang ditentukan.
+    """
+
+    random_numbers = []
+    x = seed
+    
+    for _ in range(n):
+        x = (a * x + c) % m
+        random_numbers.append(min_val + (x % (max_val - min_val + 1)))
+    
+    return random_numbers
+
+# Fungsi untuk menghasilkan nilai integer random
+def RNG(min_val, max_val): 
+    # Mendapatkan seed secara acak
+    seed = get_seed()   
+
+    a = 1103515245
+    c = 12345
+    m = 2**31
+    n = 10
+    random_sequence = linear_congruential_method_int(seed, a, c, m, n, min_val, max_val)
+
+    # Mengambil satu nilai acak dari array yang dihasilkan
+    # Menggunakan algoritma LCM untuk memilih indeks acak dari array
+    random_index = linear_congruential_method_int(seed, a, c, m, 1, 0, n-1)[0]
+    random_value = random_sequence[random_index]
+    return random_value
