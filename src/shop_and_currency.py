7caa55986ddd4fce0aaf05db43f7263c5ad7f399
unknown <19623020@std.stei.itb.ac.id>
2024-05-18 13:31:28 +0700

    fix: memperbaiki bug shop and currency

--- src/shop_and_currency.py
+++ src/shop_and_currency.py
@@ -9,24 +9,24 @@ def ClearScreen():
         # Linux and macOS
         os.system('clear')
 
-def beli_monster(monster_data, user_monster, monster_shop_data):
+def beli_monster(monster_data, user_monster, monster_shop_data, user_oc):
 
     pilih_monster = int(input("Masukkan ID monster: "))
     
     user_monster_name = []
     for i in range(1, len(user_monster)+1):
-        user_monster_name.append(user_monster[i]["Name"])
+        user_monster_name.append(user_monster[i]["Name"])  #menambahkan semua nama monster di inventory user ke array
 
     user_monster_id = []
     for i in range(len(user_monster_name)):
-        index = search_index(monster_data,"type", user_monster_name[i])
-        id = monster_data["id"][index]
-        user_monster_id.append(id)
+        index = search_index(monster_data,"type", user_monster_name[i]) #mencari index di monster data yang namanya sama dengan di user inventory 
+        id = monster_data["id"][index]              #mengakses id dari tiap nama monster dengan index yang didapat                   
+        user_monster_id.append(id)                                   
 
-    idx_name_choosen_monster= search_index(monster_data, 'id', pilih_monster)
-    name_choosen_monster= monster_data['type'][idx_name_choosen_monster]
+    idx_name_choosen_monster= search_index(monster_data, 'id', pilih_monster) #mencari indeks dari id monster yang ingin dibeli
+    name_choosen_monster= monster_data['type'][idx_name_choosen_monster] #mencari nama monster yang dipilih
 
-    if pilih_monster in user_monster_id:
+    if pilih_monster in user_monster_id: #mengecek apakah pilihan yang ingin dibeli sudah ada atau tidak di inventory
         print(f"Monster {name_choosen_monster} sudah ada dalam inventory-mu! Pembelian dibatalkan.")
 
     elif monster_shop_data["stock"] == 0: # Jika stok berjumlah 0, tidak bisa melanjutkan ke tahap berikutnya
@@ -35,7 +35,7 @@ def beli_monster(monster_data, user_monster, monster_shop_data):
         
     else: 
         index=search_index(monster_shop_data, 'monster_id', pilih_monster)
-        harga_monster = monster_shop_data["price"][index]
+        harga_monster = monster_shop_data["price"][index]   #mencari harga dari monster yang diinginkan user
 
 
         if  user_oc >= harga_monster:
@@ -46,12 +46,13 @@ def beli_monster(monster_data, user_monster, monster_shop_data):
 
             banyak_monster_di_inventory=len(user_monster)
 
-            user_monster[banyak_monster_di_inventory+1] = {'Type': 'Monster',
-                    'Name'      : name_choosen_monster,
-                    'ATK_Power' : monster_data['atk_power'][idx_name_choosen_monster],
-                    'DEF_Power' : monster_data['def_power'][idx_name_choosen_monster],
-                    'HP'        : monster_data['hp'][idx_name_choosen_monster],
-                    'Level'     : 1}
+            user_monster[banyak_monster_di_inventory+1] = {'Type': 'Monster',   #menambahkan jenis monster yang baru
+                'Name'      : name_choosen_monster,
+                'ATK_Power' : monster_data['atk_power'][idx_name_choosen_monster],
+                'DEF_Power' : monster_data['def_power'][idx_name_choosen_monster],
+                'HP'        : monster_data['hp'][idx_name_choosen_monster],
+                'Level'     : 1
+            }
             
             
 
@@ -61,7 +62,7 @@ def beli_monster(monster_data, user_monster, monster_shop_data):
     return user_monster, user_oc, monster_shop_data
         
 
-def beli_potion(item_shop_data, user_potion):
+def beli_potion(item_shop_data, user_potion, user_oc):
     pilih_potion = int(input("Masukkan ID potion: "))
     jumlah_potion = int(input("Masukkan jumlah potion: "))
 
@@ -72,15 +73,22 @@ def beli_potion(item_shop_data, user_potion):
 
         harga_potion = item_shop_data["price"] * jumlah_potion
 
-        if (user_oc == harga_potion) or ( user_oc >= harga_potion):
+        if  user_oc >= harga_potion:
             item_shop_data["stock"] = item_shop_data["stock"] - jumlah_potion  # Mengurangi jumlah stok
             user_oc = user_oc - harga_potion
-            print(f"Berhasil membeli item: {pilih_potion}. Item sudah masuk ke inventory-mu!")
+
+            idx_potion=pilih_potion-1
+            potion_name=item_shop_data['type'][idx_potion] #mengakses nama dari potion yang dipilih
+            print(f"Berhasil membeli item: Potion of {potion_name}. Item sudah masuk ke inventory-mu!")
             print(f"Jumlah O.W.C.A. Coin-mu sekarang {user_oc}")
 
-
+            for key in user_potion:
+                if user_potion[key]['Potion_Name']==potion_name:
+                    user_potion[key]['Quantity']=int(user_potion[key]['Quantity'])+jumlah_potion #menambahkan banyak potion ke inventory user
+                break
         else : #Jika uang tidak cukup, looping berhenti
             print("OC-mu tidak cukup.")
+    return user_potion, user_oc, item_shop_data
 
 
 
@@ -112,20 +120,19 @@ def shop(monster_shop_data,user_data, item_shop_data, user_monster, monster_data
                     print("ID | Type | Stok | Harga")
                     for i in range(len(item_shop_data["type"])):
                         print(f" {i+1} | {item_shop_data['type'][i]} | {item_shop_data['stock'][i]} | {item_shop_data['price'][i]}")
+                continue
 
 
         elif action_shop == "beli":
-            user_oc = user_data["oc"]
+            user_oc = int(user_data["oc"])
             print(f"Jumlah O.W.C.A. Coin-mu sekarang {user_oc}")
             beli_apa = int(input("Mau beli apa? (monster/potion) "))
             z = True
 
             if beli_apa == "monster":
-                beli_monster(monster_data, user_monster, monster_shop_data)
+                user_monster, user_oc, item_shop_data=beli_monster(monster_data, user_monster, monster_shop_data, user_oc)
             elif beli_apa == "potion":
-                beli_potion()
-            else :
-                return
-
-        else : #action_shop == "keluar"
-            return user_monster, user_potion, current_oc
+                user_potion, user_oc, item_shop_data=beli_potion(item_shop_data, user_potion, user_oc)
+            continue
+        elif action_shop=='keluar' or action_shop=='Keluar' or action_shop=='KELUAR':
+            return user_monster, user_potion, user_oc
